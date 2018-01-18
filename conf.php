75a0fc414c04123f6477502ebef3a4ae2a981b98
C0L3NzO <kaspar.jakobson@khk.ee>
2018-01-18 09:42:16 +0200

    Määratud konfiguratsioonifaili abikonstandid

---
+++ conf.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 18.01.18
+ * Time: 09:35
+ */
+
+// konfiguratsiooni fail
+
+// loome vajalikud abikonstandid
+define("MODEL_DIR", "model/");
+define("VIEWs_DIR", "views/");
+define("CONTROL_DIR", "controllers/");
