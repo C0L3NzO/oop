2a03708f4c51c1a10033e81ff6b8b4c4417ed4d9
C0L3NzO <kaspar.jakobson@khk.ee>
2018-02-02 08:52:34 +0200

    Tehtud kontrolli vaikimisi väärtuse muutmist

--- control.php
+++ control.php
@@ -12,5 +12,6 @@ if(file_exists($file) and is_file($file) and is_readable($file)){
     require_once $file;
 } else {
     $file = CONTROL_DIR.DEFAULT_CONTROL.".php";
+    $http->set("control", DEFAULT_CONTROL);
     require_once $file;
 }
